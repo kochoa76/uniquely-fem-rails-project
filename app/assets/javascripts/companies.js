687fd7230652f11d89c142fa2e7d2fa4c67badf9
github username <github email address>
2018-09-30 15:35:43 -0500

    added to app home page

---
+++ app/assets/javascripts/companies.js
@@ -0,0 +1,9 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+// 
+// $('.grid').masonry({
+//   // options...
+//   itemSelector: '.grid-item',
+//   columnWidth: 200
+// });
