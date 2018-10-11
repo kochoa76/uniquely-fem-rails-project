22943df40f8a11fbb41d466bb6455f7db73ba5d9
github username <github email address>
2018-10-11 17:30:52 -0500

    adding styling to index reviews prior to starting see next reviews javascript portion

---
+++ app/assets/javascripts/reviews.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+$(document).on("click", ".js-nextReview", function() {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+function Review(review) {
+  this.id = review.id
+  this.company_id = review.company_id
+  this.content = review.content
+  this.salary = review.salary
+  this.job_rating = review.job_rating
+  this.promo_opps = review.promo_opps
+  this.women_exec_roles = review.women_exec_roles
+  this.created_at = review.created_at
+  this.recommend = review.recommend
+}
